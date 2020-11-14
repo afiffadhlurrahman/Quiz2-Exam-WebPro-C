94d51f6f0a7791588ee48e3899b55b145a8cd4db
Raferto <rafid.ferdianto@gmail.com>
2020-11-14 20:41:23 +0700

    bug Fix

--- quiz2/quiz2/Pages/CarList/Edit.cshtml.cs
+++ quiz2/quiz2/Pages/CarList/Edit.cshtml.cs
@@ -5,16 +5,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using quiz2.Model;
+using Microsoft.AspNetCore.Identity;
+using quiz2.Areas.Identity.Data;
 
 namespace quiz2.Pages.CarList
 {
     public class EditModel : PageModel
     {
-        private ApplicationDbContext _db;
+        private readonly UserManager<UserApplication> _userManager;
+        private readonly SignInManager<UserApplication> _signInManager;
+        private readonly ApplicationDbContext _db;
 
-        public EditModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db, SignInManager<UserApplication> signInManager, UserManager<UserApplication> userManager)
         {
             _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [BindProperty]
@@ -26,7 +32,7 @@ namespace quiz2.Pages.CarList
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && _userManager.GetUserName(User) == Car.Creator)
             {
                 var CarFromDb = await _db.Car.FindAsync(Car.Id);
                 CarFromDb.Name = Car.Name;
--- quiz2/quiz2/Pages/CarList/Index.cshtml.cs
+++ quiz2/quiz2/Pages/CarList/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using quiz2.Areas.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,11 +13,15 @@ namespace quiz2.Pages.CarList
 {
     public class IndexModel : PageModel
     {
+        private readonly UserManager<UserApplication> _userManager;
+        private readonly SignInManager<UserApplication> _signInManager;
         private readonly ApplicationDbContext _db;
 
-        public IndexModel(ApplicationDbContext db)
+        public IndexModel(ApplicationDbContext db, SignInManager<UserApplication> signInManager, UserManager<UserApplication> userManager)
         {
             _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IEnumerable<Car> Cars { get; set; }
@@ -27,12 +33,15 @@ namespace quiz2.Pages.CarList
         public async Task<IActionResult> OnPostDelete(int id)
         {
             var car = await _db.Car.FindAsync(id);
-            if(car == null)
+            if (car == null)
             {
                 return NotFound();
             }
-            _db.Car.Remove(car);
-            await _db.SaveChangesAsync();
+            if(car.Creator == _userManager.GetUserName(User))
+            {
+                _db.Car.Remove(car);
+                await _db.SaveChangesAsync();
+            }
             return RedirectToPage("Index");
         }
     }
